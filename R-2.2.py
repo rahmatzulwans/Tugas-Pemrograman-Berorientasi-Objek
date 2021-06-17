bad65aea33f47ba1e2f3964bc198e92709772d9a
rahmatzulwans <58312080+rahmatzulwans@users.noreply.github.com>
2021-06-17 14:18:05 +0800

    Add files via upload

---
+++ R-2.2.py
@@ -0,0 +1,14 @@
+#Perangkat lunak efek visual dan animasi
+
+#Perangkat Lunak efek visual dan perangkat lunak animasi harus sangat mudah beradaptasi
+#karena evolusi cepat yang terjadi di sektor ini.
+#Film aksi langsung menghabiskan lebih sedikit waktu dalam pasca produksi,
+#rendering berbasis GPU sedang meningkat dan teknologi canggih
+#seperti penangkapan gerak diadopsi secara luas.
+#Ada juga peningkatan VR dan AR dan permintaan untuk pengalaman bermain game
+#yang lebih realistis. Setiap perangkat lunak VFX harus dapat menggabungkan
+#teknologi baru ini atau akan tertinggal. Jika rangkaian pengembangan VR,
+#perender, mesin permainan, atau ekstensi atau plugin semacam itu menjadi
+#populer, perusahaan perangkat lunak VFX harus dapat dengan cepat memperbarui
+#perangkat lunaknya untuk memungkinkannya mendukung plugin baru ini.
+
